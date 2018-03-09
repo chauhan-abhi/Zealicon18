969f5121c1ef0680a8720f4b7de923ab8d1d4e8c
mrsinghania <abhisinghania14@gmail.com>
2018-03-09 08:52:50 +0530

    event detail page

--- app/src/main/java/com/example/abhi/jsshndemo/activities/EventDetailsActivity.java
+++ app/src/main/java/com/example/abhi/jsshndemo/activities/EventDetailsActivity.java
@@ -1,17 +1,31 @@
 package com.example.abhi.jsshndemo.activities;
 
 import android.Manifest;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.graphics.Typeface;
 import android.net.Uri;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SwitchCompat;
+import android.util.Log;
 import android.view.View;
+import android.widget.CompoundButton;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import com.example.abhi.jsshndemo.R;
-import com.example.abhi.jsshndemo.model.InnerData;
+import com.example.abhi.jsshndemo.service.NotificationService;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class EventDetailsActivity extends AppCompatActivity {
 
@@ -21,6 +35,7 @@ public class EventDetailsActivity extends AppCompatActivity {
   private TextView secPrize;
   private TextView contact;
   private ImageButton callButton;
+  private SwitchCompat interestedSwitch,goingSwitch;
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -32,7 +47,8 @@ public class EventDetailsActivity extends AppCompatActivity {
     secPrize = findViewById(R.id.text_event_details_secondprize);
     contact = findViewById(R.id.text_event_details_contact);
     callButton = findViewById(R.id.callbutton_eventdetails);
-
+    interestedSwitch=findViewById(R.id.switchcompat_interested);
+    goingSwitch=findViewById(R.id.switchcompat_going);
 
     //*************Dummy Data***************//
     eventName.setText("Technovision");
@@ -60,7 +76,58 @@ public class EventDetailsActivity extends AppCompatActivity {
       }
     });
 
+    goingSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+      @Override
+      public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+        if(b){
+          notifyme();
+          Toast.makeText(getBaseContext(),"We will notify you..",Toast.LENGTH_SHORT).show();
+        }
+        else {
+          canclenotifyme();
+        }
+      }
+    });
     //Getting values from shared prefrences
 
+  }
+
+  private void canclenotifyme() {
+    Intent intent = new Intent(this, NotificationService.AlarmReceiver.class);
+    int id=getSharedPreferences("notify",0).getInt("eventname",1);
+    PendingIntent sender = PendingIntent.getBroadcast(this,id , intent, PendingIntent.FLAG_ONE_SHOT);
+    AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+
+    alarmManager.cancel(sender);
+  }
+
+  private void notifyme() {
+    String toParse = ""; // Results in "2-5-2012 20:43"
+    SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss"); // I assume d-M, you may refer to M-d for month-day instead.
+    Date date = null; // You will need try/catch around this
+    try {
+      date = formatter.parse(toParse);
+    } catch (ParseException e) {
+      e.printStackTrace();
+    }
+    SharedPreferences sf=getSharedPreferences("notify",0);
+    int notificationid=sf.getInt("key",1);
+    sf.edit().putInt("eventname",notificationid).commit();
+    long millis = date.getTime();
+    Intent intent=new Intent(getApplicationContext(), NotificationService.AlarmReceiver.class);
+    intent.putExtra("keynotify", notificationid);
+    intent.putExtra("eventname","eventname");
+    Log.v("keynotify1", notificationid + "");
+
+
+    PendingIntent pi=PendingIntent.getBroadcast(getApplicationContext(),notificationid,intent,PendingIntent.FLAG_ONE_SHOT);
+    sf.edit().putInt("key",notificationid+1).commit();
+    AlarmManager am=(AlarmManager)getApplicationContext().getSystemService(Context.ALARM_SERVICE);
+    am.set(AlarmManager.RTC_WAKEUP,millis-900000,pi);
+  }
+
+  private void settextstyle(TextView tv1) {
+    Typeface custom_font = Typeface.createFromAsset(getApplicationContext().getAssets(), "font/huggable.ttf");
+    tv1.setTypeface(custom_font);
   }
 }
--- app/src/main/java/com/example/abhi/jsshndemo/activities/SplashActivity.java
+++ app/src/main/java/com/example/abhi/jsshndemo/activities/SplashActivity.java
@@ -1,9 +1,16 @@
 package com.example.abhi.jsshndemo.activities;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
 import com.example.abhi.jsshndemo.R;
 import net.grandcentrix.tray.AppPreferences;
 
@@ -25,7 +32,34 @@ public class SplashActivity extends AppCompatActivity {
   }
 
   private void requestjson() {
-    final String backofficeUrl = "";
+
+    final String backofficeUrl = "http://backoffice.zealicon.in/events";
+    final StringRequest stringRequestcoderz = new StringRequest(Request.Method.GET, backofficeUrl+"0",
+            new Response.Listener<String>() {
+              @Override
+              public void onResponse(String response) {
+
+                Log.v("MyApp", response);
+                SharedPreferences s=getSharedPreferences("events",0);
+                s.edit().putString("allevents",response).apply();
+                SharedPreferences sf=getSharedPreferences("firsttime",0);
+                sf.edit().putInt("first", 1).apply();
+                Intent in =new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(in);
+
+
+              }
+            },
+            new Response.ErrorListener() {
+              @Override
+              public void onErrorResponse(VolleyError error) {
+                //You can handle error here if you want
+                Log.v("MyApp",error.toString());
+                //Intent in=new Intent(getApplicationContext(),Retry.class);
+                //startActivity(in);
+              }
+            });
+
     //final RequestQueue requestQueue = Volley.newRequestQueue(this);
     new Handler().postDelayed(new Runnable() {
       @Override public void run() {
